8dbdd9889aa21856c1e448fe4970fa571635f2b1
Samuel Lorand <samuel.lorand@hotmail.com>
2026-04-28 22:31:39 -0300

    feat: pagina inicial simples do frontend

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+function App() {
+  return (
+    <div style={{ textAlign: "center", marginTop: "50px" }}>
+      <h1>Em breve: EventHub</h1>
+      <p>Sistema de gerenciamento de eventos</p>
+    </div>
+  )
+}
+
+export default App
